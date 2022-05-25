78ef28a1fe339f03b1cba01d54870fa32b018020
DafterWatch <marcosingefa@gmail.com>
2022-05-25 02:15:28 -0400

    interfaces ++++

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DominiosComponent } from './components/dominios/dominios.component';
+import { LoginComponent } from './components/login/login.component';
+import { PagprincipalComponent } from './components/pagprincipal/pagprincipal.component';
+import { ProcesosComponent } from './components/procesos/procesos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent },
+  { path: 'pagprincipal', component: PagprincipalComponent },
+  { path: 'dominios', component: DominiosComponent },
+  { path: 'procesos', component: ProcesosComponent },
+  { path: '**', redirectTo: 'login', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PagprincipalComponent } from './components/pagprincipal/pagprincipal.component';
+import { LoginComponent } from './components/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DominiosComponent } from './components/dominios/dominios.component';
+import { ProcesosComponent } from './components/procesos/procesos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PagprincipalComponent,
+    LoginComponent,
+    DominiosComponent,
+    ProcesosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
